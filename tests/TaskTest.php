df1818b971ed5c986e9fdaf452523d63fdc55d67
Pavel <pavelpeper@gmail.com>
2026-03-16 03:33:26 +0300

    added autoload

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,8 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
-
-require_once __DIR__ . '/../src/Task.php';
+use App\Task;
 
 class TaskTest extends TestCase
 {
@@ -11,6 +11,8 @@ class TaskTest extends TestCase
         $task = new Task(authorId: 1);
 
         $this->assertSame(Task::STATUS_NEW, $task->getStatus());
+        $this->assertSame(1, $task->getAuthorId());
+        $this->assertNull($task->getExecutorId());
     }
 
     public function testConstructorSetsStatus(): void
@@ -21,6 +23,17 @@ class TaskTest extends TestCase
         );
 
         $this->assertSame(Task::STATUS_ACTIVE, $task->getStatus());
+    }
+
+    public function testConstructorSetsExecutorId(): void
+    {
+        $task = new Task(
+            authorId: 1,
+            status: Task::STATUS_NEW,
+            executorId: 13
+        );
+
+        $this->assertSame(13, $task->getExecutorId());
     }
 
     public function testGetNextStatusFromNew(): void
@@ -60,15 +73,14 @@ class TaskTest extends TestCase
     {
         $task = new Task(authorId: 1);
 
-        $this->assertNull(
-            $task->getNextStatus(Task::ACTION_COMPLETE)
-        );
+        $this->assertNull($task->getNextStatus(Task::ACTION_COMPLETE));
     }
 
     public function testGetAllowedActionsForNew(): void
     {
         $actions = Task::getAllowedActions(Task::STATUS_NEW);
 
+        $this->assertCount(3, $actions);
         $this->assertContains(Task::ACTION_START, $actions);
         $this->assertContains(Task::ACTION_CANCEL, $actions);
         $this->assertContains(Task::ACTION_RESPOND, $actions);
@@ -78,6 +90,7 @@ class TaskTest extends TestCase
     {
         $actions = Task::getAllowedActions(Task::STATUS_ACTIVE);
 
+        $this->assertCount(2, $actions);
         $this->assertContains(Task::ACTION_COMPLETE, $actions);
         $this->assertContains(Task::ACTION_FAIL, $actions);
     }
@@ -93,17 +106,24 @@ class TaskTest extends TestCase
     {
         $statuses = Task::getStatuses();
 
+        $this->assertCount(5, $statuses);
         $this->assertArrayHasKey(Task::STATUS_NEW, $statuses);
         $this->assertArrayHasKey(Task::STATUS_ACTIVE, $statuses);
         $this->assertArrayHasKey(Task::STATUS_COMPLETED, $statuses);
+        $this->assertArrayHasKey(Task::STATUS_CANCELED, $statuses);
+        $this->assertArrayHasKey(Task::STATUS_FAILED, $statuses);
     }
 
     public function testGetActions(): void
     {
         $actions = Task::getActions();
 
+        $this->assertCount(5, $actions);
+
         $this->assertArrayHasKey(Task::ACTION_START, $actions);
         $this->assertArrayHasKey(Task::ACTION_CANCEL, $actions);
         $this->assertArrayHasKey(Task::ACTION_RESPOND, $actions);
+        $this->assertArrayHasKey(Task::ACTION_COMPLETE, $actions);
+        $this->assertArrayHasKey(Task::ACTION_FAIL, $actions);
     }
 }
